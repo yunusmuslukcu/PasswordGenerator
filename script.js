97311de6d8b721341c9c93c6f1729bfc4a7a2b17
Yunus Muslukcu <ornek@email.com>
2025-08-26 14:38:43 +0300

    PasswordGeneratorProject

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordButton = document.querySelector(".passwordButton");
+const input = document.querySelector("input");
+const faCopy = document.querySelector(".fa-copy");
+const alertContainer = document.querySelector(".alertContainer");
+
+passwordButton.addEventListener("click", createPassword);
+faCopy.addEventListener("click", copyPassword);
+
+function createPassword() {
+  const passwordLength = 14;
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+-={}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let password = "";
+  for (let index = 0; index < passwordLength; index++) {
+    const randomNum = Math.floor(Math.random() * characters.length);
+    password += characters[randomNum];
+  }
+  input.value = password;
+  alertContainer.textContent = password + " Kopyalandı! ";
+}
+function copyPassword() {
+  if (input.value) {
+    input.select();
+    navigator.clipboard.writeText(input.value);
+    alertContainer.classList.remove("active");
+    setTimeout(() => {
+      alertContainer.classList.add("active");
+    }, 3000);
+  }
+}
